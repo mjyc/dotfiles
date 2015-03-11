69e5de80d356da2d5ac386396a0c4f571b1610a1
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2015-03-10 21:08:20 -0700

    mac updates

---
+++ brew-cask.sh
@@ -0,0 +1,30 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install alfred
+# brew cask install divvy  # use App Sore
+brew cask install dropbox
+brew cask install gyazo
+brew cask install rescuetime
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text3
+# brew cask install imagealpha
+# brew cask install imageoptim
+
+# browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install chromium --force
+
+# less often
+brew cask install gimp
+brew cask install github
+brew cask install mou
+brew cask install vlc
+brew cask install yed
+
