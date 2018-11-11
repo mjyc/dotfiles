3678a79ab66c18b6831f8f8a68f436701a59713a
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-11-10 20:45:29 -0800

    Add visual studio code

--- brew-cask.sh
+++ brew-cask.sh
@@ -5,6 +5,7 @@ brew tap caskroom/cask
 brew cask install dropbox
 brew cask install iterm2
 brew cask install sublime-text
+brew cask install visual-studio-code
 # browsers
 brew cask install google-chrome
 brew cask install chromium --force
