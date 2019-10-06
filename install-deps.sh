c29306b050176c9519ccfd7866816aea4949c464
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-10-06 08:58:22 -0700

    Remove comment

--- install-deps.sh
+++ install-deps.sh
@@ -1,22 +1,15 @@
-# up to you (me) if you want to run this as a file or copy paste at your leisure
-# do `bash install-deps.sh`
-
-
-# homebrew!
+# homebrew
 # if [[ "$OSTYPE" =~ ^darwin ]]; then
   # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #fi
 
 
-# https://github.com/rupa/z
-# z, oh how i love you
+# directory jumper https://github.com/rupa/z
 if [ ! -d ~/local/src ]; then mkdir -p ~/local/src; fi;
 cd ~/local/src
 git clone https://github.com/rupa/z.git
 chmod +x ~/local/src/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-# z binary is already referenced from .bash_profile
 
 
 # for the c alias (syntax highlighted cat)
