d211c647d1f05c6e4230402fc93df50cdde69d34
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-11-22 10:53:57 -0800

    Add poppler

--- brew.sh
+++ brew.sh
@@ -47,6 +47,7 @@ brew install tmux
 brew install ssh-copy-id
 brew install ffmpeg --with-libvpx
 brew install icdiff
+brew install poppler
 
 # Remove outdated versions from the cellar
 brew cleanup
