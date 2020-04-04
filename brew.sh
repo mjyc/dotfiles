df7f3876a31341f675a92b61e0105b068fbaf68f
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-04-04 12:39:15 -0700

    Add git-issue set up scripts

--- brew.sh
+++ brew.sh
@@ -10,18 +10,19 @@ brew update
 # Install GNU core utilities
 # - don't forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`
 brew install coreutils
-# Install some other useful utilities like `sponge`
-brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # Install GNU `sed` to overwrite the built-in `sed`
 brew install gnu-sed
-# Install wget with IRI support
-brew install wget
-# Install more linux utilities
-brew install grep
+# Install more GNU
+brew install grep utilities
 brew install gnu-tar
 brew install gzip
+# Install some other useful utilities like `sponge`
+brew install moreutils
+# Install wget with IRI support
+brew install wget
+# Install more utilities
 brew install watch
 brew install rename
 brew install tree
@@ -29,6 +30,7 @@ brew install tree
 # - don't forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`
 brew install bash
 brew install bash-completion
+
 # Install dev tools
 brew install vim
 # brew install macvim # conflicts with vim
@@ -38,7 +40,7 @@ brew install imagemagick
 # brew install node # use nvm instead
 brew install pv
 brew install trash
-brew install reattach-to-user-namespace # must before tmux
+brew install reattach-to-user-namespace # must be run before tmux
 brew install tmux
 brew install ssh-copy-id
 brew install ffmpeg
--- install-deps.sh
+++ install-deps.sh
@@ -12,6 +12,16 @@ chmod +x ~/local/src/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 
+# git-issue
+git clone  https://github.com/dspinellis/git-issue.git
+# add the following in
+# export GIT_ISSUE_LIB_PATH="$HOME/local/src/git-issue/lib"
+# export GL_CURL_AUTH="PRIVATE-TOKEN: {gitlab token}"
+# export GH_CURL_AUTH="Authorization: token  {github token}"
+# export PAGER="cat"
+# source $HOME/local/src/git-issue/gi-completion.sh
+
+
 # for the c alias (syntax highlighted cat)
 if [[ "$OSTYPE" =~ ^darwin ]]; then
   sudo easy_install Pygments
