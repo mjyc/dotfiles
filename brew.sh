0ff1c3dad4bbeba17fe5118e1b0623df0bc22057
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-12-22 13:19:51 -0800

    Update brew

--- brew.sh
+++ brew.sh
@@ -1,51 +1,26 @@
+# IMPORTANT! doesn't work for M1
+
 #!/bin/bash
-# http://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
 
 # Make sure we're using the latest Homebrew
-brew update
+# brew update
 
 # Upgrade any already-installed formulae
 # brew upgrade
 
-# Install GNU core utilities
-# IMPORTANT!! don't forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`
-# brew install coreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-# brew install findutils
-# Install GNU `sed` to overwrite the built-in `sed`
-# brew install gnu-sed
-# Install more GNU
-# brew install grep utilities
-# brew install gnu-tar
-# brew install gzip
-# Install some other useful utilities like `sponge`
-# brew install moreutils
-# Install wget with IRI support
-# brew install wget
-# Install more utilities
-# brew install watch
-# brew install rename
-# brew install tree
-# Install Bash 5
-# - don't forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`
-# brew install bash
-# brew install bash-completion
-
 # Install dev tools
-brew install vim
-# brew install macvim # conflicts with vim
-brew install git
-brew install hub
-brew install imagemagick
-# brew install node # use nvm instead
-# brew install pv  # TODO: reconsider
-brew install trash
-# brew install reattach-to-user-namespace # must be run before tmux  # TODO: reconsider
-brew install tmux
-# brew install ssh-copy-id  # TODO: reconsider
-# brew install ffmpeg  # TODO: reconsider
-brew install icdiff
-# brew install poppler  # TODO: reconsider
+# http://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+# brew install vim  # use system vim
+# brew install git  # use system vim
+# brew install hub
+# brew install node  # use nvm instead
+# brew install reattach-to-user-namespace # must be run before tmux
+# brew install tmux
+# brew install trash
+# brew install icdiff
+# brew install imagemagick
+# brew install ssh-copy-id
+# brew install poppler
 
 # Remove outdated versions from the cellar
-brew cleanup
+# brew cleanup
