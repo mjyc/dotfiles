ced880ece4cbec51fa5ef628c52b2ca62e9089a8
Mike C <mjyc@users.noreply.github.com>
2023-06-27 10:40:29 -0700

    Update install-devcontainer.sh

--- install-devcontainer.sh
+++ install-devcontainer.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
 if [[ "$OSTYPE" =~ ^linux ]]; then
 	sudo apt install -y vim
 fi
-echo 'alias g="git"' >> .bashrc
-echo 'alias v="vim"' >> .bashrc
+
+# Sync dotfiles
+for file in ~/.exports ~/.inputrc ~/.vimrc;
+	do cp "$file" ~/;
+done
+# Add aliases
+for alias in 'alias g="git"' 'alias v="vim"';
+	do echo "$alias" >> .bashrc
+done
+
+cat <<- EOF > ~/.gitignore
+.DS_Store
+Desktop.ini
+EOF
