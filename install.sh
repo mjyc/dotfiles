bdefcc314091ddcd1d618e75f3d7f8249155aa99
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-10-08 16:57:27 -0700

    Update install devcontainer detection logic

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-if [[ "$USER" == "codespace" || "$USER" == "vscode" ]]; then
-  ./install-devcontainer.sh
-else
+if [[ "$USER" == "mjyc" ]]; then
   if [[ "$OSTYPE" =~ ^darwin ]]; then
     # macOS
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -10,4 +8,6 @@ else
     ./apt.sh
   fi
   ./sync.sh -f
+else
+  ./install-devcontainer.sh
 fi
