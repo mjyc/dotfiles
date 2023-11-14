701645adc839d3de5c1bff71b267955bc4e0d8f1
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-11-13 21:15:21 -0800

    Add more aliases for devcontainer

--- install-devcontainer.sh
+++ install-devcontainer.sh
@@ -3,41 +3,23 @@
 # Install dev tools
 if [[ "$OSTYPE" =~ ^linux ]]; then
 	export DEBIAN_FRONTEND=noninteractive
-	sudo apt update && sudo apt install -y --no-install-recommends vim icdiff entr
+	sudo apt update && sudo apt install -y --no-install-recommends vim icdiff entr ripgrep fd-find
 fi
 
 # Sync dotfiles
-for file in .exports .inputrc .vimrc .tmux.conf;
+for file in .exports .aliases-devcontainer .inputrc .vimrc .tmux.conf;
 	do cp "$file" ~/
 done
-for folders in .vim .tmux;
-	do cp -r "$folders" ~/
+unset file
+for folder in .vim .tmux;
+	do cp -r "$folder" ~/
 done
+unset folder
 
-# Add aliases
-# my favorites
-echo 'alias g="git"' >> ~/.bashrc
-echo 'alias v="vim"' >> ~/.bashrc
-
-# ls aliases
-echo 'alias ls="ls --color=auto"' >> ~/.bashrc
-echo 'alias l="ls -F1"' >> ~/.bashrc
-echo 'alias la="ls -F1a"' >> ~/.bashrc
-echo 'alias ll="ls -l"' >> ~/.bashrc
-echo 'alias lla="ll -a"' >> ~/.bashrc
-echo 'alias llt="ll -t"' >> ~/.bashrc
-# git aliases
-echo 'alias gst="g st"' >> ~/.bashrc
-echo 'alias gbr="g br"' >> ~/.bashrc
-echo 'alias gd="g d"' >> ~/.bashrc
-echo 'alias gdf="g df"' >> ~/.bashrc
-echo 'alias gl="g l"' >> ~/.bashrc
-echo 'alias glg="g lg"' >> ~/.bashrc
-echo 'alias gau="g add -u"' >> ~/.bashrc
-echo 'alias ga.="g add ."' >> ~/.bashrc
-echo 'alias gcim="g ci -m"' >> ~/.bashrc
-echo 'alias gcia="g ci --amend"' >> ~/.bashrc
-echo 'alias gciaa="g ci -a --amend"' >> ~/.bashrc
+echo 'for file in ~/.{exports,aliases-devcontainer}; do
+    [ -r "$file" ] && source "$file"
+done
+unset file' >> ~/.bashrc
 
 # Create .gitignore
 cat << EOF >> ~/.gitignore
