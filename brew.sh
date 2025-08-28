0e98df5882d777fff7460fd0c4e060bcb011f303
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2025-08-27 19:37:55 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -10,14 +10,13 @@ brew upgrade
 # http://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
 # brew install vim  # use system vim
 # brew install git  # use system git
-brew install zoxide
+brew install z
 brew install tmux
 brew install trash-cli
 brew install icdiff
 brew install fzf
 brew install rigrep
 brew install fd
-brew install exa
 
 # Remove outdated versions from the cellar
 brew cleanup
