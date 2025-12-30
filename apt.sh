0f358c608cd93c05b0eee57b735c1ef15c1b3464
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2025-12-30 13:34:14 -0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -6,12 +6,13 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Install!
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
+sudo apt install -y --no-install-recommends openssh-server net-tools curl git vim trash-cli tmux xclip icdiff fzf ripgrep
 
-# Essentials
-sudo apt install -y --no-install-recommends openssh-server net-tools curl git vim trash-cli tmux xclip icdiff
-# TODO: scriptize "zoxide" installation
-
-# Dev tools
-sudo apt install -y --no-install-recommends fzf ripgrep fd-find exa
+# Install z https://github.com/rupa/z
+if [ ! -d ~/.local/src/ ]; then mkdir -p ~/.local/src/; fi;
+cd ~/.local/src/
+git clone https://github.com/rupa/z.git
+chmod +x ~/.local/src/z/z.sh
